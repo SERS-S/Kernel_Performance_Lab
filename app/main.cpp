3c8feeff84a9285a2c0c1957e8e0198028ae8982
SER <104014082+SERS-S@users.noreply.github.com>
2026-04-18 22:56:08 +0300

    resolve_path_func

---
+++ app/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stdexcept>
+#include <filesystem>
+
+std::filesystem::path resolve_path(const char *path)
+{
+    if (path == nullptr || path[0] == '\0')
+    {
+        throw std::invalid_argument("Path is incorrect");
+    }
+
+    const std::filesystem::path source_path = std::filesystem::absolute(path);
+    
+    if (!std::filesystem::exists(source_path))
+    {
+        throw std::invalid_argument("Path does not exist");
+    }
+
+    if (!std::filesystem::is_regular_file(source_path))
+    {
+        throw std::invalid_argument("Path is not a regular file");
+    }
+
+    const auto ext = source_path.extension().string();
+    if (ext != ".c" && ext != ".cpp")
+    {
+        throw std::invalid_argument("Path is not a C/C++ source file");
+    }
+    
+    return source_path;
+}
+
+int main(int argc, char** argv)
+{
+    std::cout << "### Kernel Performance Lab started ###" << std::endl;
+
+    try
+    {
+        if (argc < 2)
+        {
+            throw std::invalid_argument("Usage: kpl <path-to-c-or-cpp-file>");
+        }
+
+        const char* path = argv[1];
+        const auto source_path = resolve_path(path);
+        std::cout << "Source file: " << source_path << std::endl;
+
+
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << "Error: " << ex.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
